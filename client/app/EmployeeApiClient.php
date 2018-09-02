6386029e7f7da1bd7c8a0dbe84a02a7878b18f6a
appkr <juwonkim@me.com>
2018-09-02 11:56:26 +0900

    Employee API 클라이언트에 GuzzleClient 주입

--- client/app/EmployeeApiClient.php
+++ client/app/EmployeeApiClient.php
@@ -2,8 +2,25 @@
 
 namespace App;
 
+use GuzzleHttp\ClientInterface;
+use Psr\Log\LoggerInterface;
+
 class EmployeeApiClient
 {
+    private $httpClient;
+    private $config;
+    private $logger;
+
+    public function __construct(
+        ClientInterface $httpClient,
+        EmployeeApiConfiguration $config,
+        LoggerInterface $logger
+    ) {
+        $this->httpClient = $httpClient;
+        $this->config = $config;
+        $this->logger = $logger;
+    }
+
     public function listEmployees()
     {
         $json = file_get_contents('http://foo:bar@localhost:8000');
--- client/app/Providers/AppServiceProvider.php
+++ client/app/Providers/AppServiceProvider.php
@@ -2,13 +2,51 @@
 
 namespace App\Providers;
 
+use App\EmployeeApiClient;
+use App\EmployeeApiConfiguration;
 use App\EmployeeRepository;
 use App\RestEmployeeRepository;
+use GuzzleHttp\Client as GuzzleClient;
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register()
+    {
+        $this->bindEmployeeApiConfiguration();
+        $this->bindEmployeeApiClient();
+        $this->bindEmployeeRepository();
+    }
+
+    private function bindEmployeeApiConfiguration()
+    {
+        $this->app->bind(EmployeeApiConfiguration::class, function (Application $app) {
+            $config = $app->make(ConfigRepository::class);
+            $credentials = $config->get('employee.credentials');
+            $basicAuthString = base64_encode("{$credentials['client_id']}:{$credentials['client_secret']}");
+
+            return new EmployeeApiConfiguration($config->get('employee.host'), [
+                'Accept' => 'application/json',
+                'Authorization' => "Basic {$basicAuthString}",
+            ]);
+        });
+    }
+
+    private function bindEmployeeApiClient()
+    {
+        $this->app->bind(EmployeeApiClient::class, function (Application $app) {
+            $httpClient = new GuzzleClient();
+            $config = $app->make(EmployeeApiConfiguration::class);
+            $logger = $app->make(LoggerInterface::class);
+
+            return new EmployeeApiClient($httpClient, $config, $logger);
+        });
+    }
+
+    private function bindEmployeeRepository()
     {
         $this->app->bind(EmployeeRepository::class, RestEmployeeRepository::class);
     }
