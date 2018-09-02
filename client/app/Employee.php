f91930a3c5679643b5183249a30988e9241d309d
appkr <juwonkim@me.com>
2018-09-02 09:36:59 +0900

    Employee 엘로퀀트 모델 추가

---
+++ client/app/Employee.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Employee
+ * @package App
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Employee extends Model
+{
+    //
+}
