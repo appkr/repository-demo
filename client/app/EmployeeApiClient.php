3443074b38b3c808fd6c67556e32a9fd42ced920
appkr <juwonkim@me.com>
2018-09-02 12:19:58 +0900

    file_get_contents 함수를 GuzzleClient로 교체

--- client/app/EmployeeApiClient.php
+++ client/app/EmployeeApiClient.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
 use Psr\Log\LoggerInterface;
 
 class EmployeeApiClient
@@ -23,9 +25,59 @@ class EmployeeApiClient
 
     public function listEmployees()
     {
-        $json = file_get_contents('http://foo:bar@localhost:8000');
-        $decoded = json_decode($json, true);
+        $response = $this->sendRequest();
 
-        return $decoded['data'] ?? [];
+        return $response['data'] ?? [];
+    }
+
+    /**
+     * @param string $method
+     * @param string $endpoint
+     * @param array $headers
+     * @param string $httpBody
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function sendRequest(
+        string $method = 'GET',
+        string $endpoint = '/',
+        array $headers = [],
+        string $httpBody = ''
+    ) {
+        $headers = array_merge($this->config->getHeaders(), $headers);
+        $request = new Request($method, "{$this->config->getHost()}{$endpoint}", $headers, $httpBody);
+        $logContext = [
+            'request' => [
+                'endpoint' => "{$method} {$endpoint}",
+                'headers' => $headers,
+                'body' => $httpBody,
+            ],
+        ];
+
+        try {
+            $response = $this->httpClient->send($request);
+        } catch (RequestException $e) {
+            $message = $e->getMessage();
+            $errorContent = $e->getResponse()->getBody()->getContents() ?? null;
+            if ($errorContent) {
+                \GuzzleHttp\json_decode($errorContent, true);
+            }
+
+            $this->logger->error("[EmployeeApiClient] 오류가 발생했습니다.", array_merge($logContext, [
+                'exception' => [
+                    'code' => $e->getCode(),
+                    'message' => $message,
+                ],
+            ]));
+
+            throw $e;
+        }
+        $responseBody = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+
+        $this->logger->debug("[EmployeeApiClient] API 서버에서 응답을 수신했습니다.", array_merge($logContext, [
+            'response' => $responseBody,
+        ]));
+
+        return $responseBody;
     }
 }
--- client/app/Exceptions/Handler.php
+++ client/app/Exceptions/Handler.php
@@ -4,50 +4,33 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
     public function render($request, Exception $exception)
     {
+        if ($request->wantsJson()) {
+            return new JsonResponse([
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ], $exception->getCode() ?: Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
         return parent::render($request, $exception);
     }
 }
